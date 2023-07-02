a484f3f94a27fe011d63bdeebe96713530633d75
Dmitrii Kopasov <58292035+mandarin-coffee@users.noreply.github.com>
2023-07-02 18:17:42 +0300

    Feat: добавление eslint & prettier и установка стандарта на двойные ковычки

--- public/scripts/test/sum.test.js
+++ public/scripts/test/sum.test.js
@@ -1,7 +1,7 @@
-import {sum} from '../sum.js';
+import { sum } from "../sum.js";
 
-describe('sum', () => {
-    it('1 + 2 = 3', () => {
-        expect(sum(1,2)).toBe(3);
-    });
-})
+describe("sum", () => {
+  it("1 + 2 = 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
