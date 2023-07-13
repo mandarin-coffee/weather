90b0cdf34743ea2c5912662cf5f6ec2a22105fed
Dmitrii Kopasov <58292035+mandarin-coffee@users.noreply.github.com>
2023-07-13 21:53:36 +0300

    Feat (webpack-config): Подключение и настройка webpack

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-};
+// module.exports = {
+//   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+// };
+module.exports = (api) => ({
+  // plugins: [
+  //   "@babel/plugin-proposal-nullish-coalescing-operator",
+  //   "@babel/plugin-proposal-optional-chaining"
+  // ],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        // caller.target will be the same as the target option from webpack
+        corejs: "3.22",
+        targets: api.caller((caller) => caller && caller.target === "node")
+          ? { node: "current" }
+          : { chrome: "58", ie: "11" },
+      },
+    ],
+  ],
+});
---
+++ public/index.js
@@ -0,0 +1,3 @@
+import "./styles/test.scss";
+
+console.log(1);
